701c73a6aa0e2459fecc3925917c3c0d513e9d0a
ju4n53 <juansegutierrez7@gmail.com>
2026-05-31 19:26:30 -0300

    Cambios: - Vehiculo: DB-Sp + API-Cs y Controller

---
+++ WebApi/API_SITMAS/Controllers/VehiculoController.cs
@@ -0,0 +1,103 @@
+﻿using API_SITMAS.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_SITMAS.Controllers
+{
+    public class VehiculoController : ApiController
+    {
+        // GET: api/Vehiculo
+        [HttpGet]
+        public List<Vehiculo> ListarTodo()
+        {
+            Vehiculo oVehiculo = new Vehiculo();
+
+            DataTable dt = oVehiculo.SelectAll();
+            var listaJson = JsonConvert.SerializeObject(dt);
+
+            var Lista = JsonConvert.DeserializeObject<List<Vehiculo>>(listaJson);
+
+            return Lista;
+
+        }
+
+        // GET: api/Area/5
+        //[HttpGet]
+
+        //public Empleado ListarPorId(int id)
+        //{
+        //    Empleado oEmpleado = new Empleado();
+        //    oEmpleado.Id = id;
+
+        //    DataTable dt = oEmpleado.SelectId();
+
+        //    var ListaJsom = JsonConvert.SerializeObject(dt);
+
+        //    var obj = JsonConvert.DeserializeObject<List<Empleado>>(ListaJsom).ToList().FirstOrDefault();
+
+        //    return obj;
+
+        //}
+
+        // POST: api/Vehiculo
+        [HttpPost]
+        public void Insertar([FromBody] Vehiculo value)
+        {
+            Vehiculo oVehiculo = new Vehiculo();
+            oVehiculo.Patente = value.Patente;
+            oVehiculo.Id_Modelo = value.Id_Modelo;
+            oVehiculo.Id_Tipo = value.Id_Tipo;
+
+            oVehiculo.Insertar();
+        }
+
+        // PUT: api/Vehiculo/5
+        [HttpPost]
+        public void Modificar(int id, [FromBody] Vehiculo value)
+        {
+
+            Vehiculo oVehiculo = new Vehiculo();
+            oVehiculo.Id = id;
+            oVehiculo.Patente = value.Patente;
+            oVehiculo.Id_Modelo = value.Id_Modelo;
+            oVehiculo.Id_Tipo = value.Id_Tipo;
+
+            oVehiculo.Modificar();
+
+        }
+
+        // DELETE: api/Vehiculo/5
+        [HttpPost]
+
+        public void Borrar(int id)
+        {
+
+            Vehiculo oVehiculo = new Vehiculo();
+            oVehiculo.Id = id;
+
+            oVehiculo.Borrar();
+
+        }
+
+        //[HttpGet]
+        //public List<Empleado> ListarVista()
+        //{
+        //    Empleado oEmpleado = new Empleado();
+
+        //    var dt = oEmpleado.VistalistadoEmpleados();
+
+        //    var ListaJsom = JsonConvert.SerializeObject(dt);
+
+        //    var Lista = JsonConvert.DeserializeObject<List<Empleado>>(ListaJsom);
+        //    return Lista;
+
+        //}
+
+    }
+}
---
+++ WebApi/API_SITMAS/Models/Vehiculo.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace API_SITMAS.Models
+{
+    public class Vehiculo
+    {
+        #region Atributos
+
+        // Ahora le pedimos al ConfigurationManager que busque la cadena por su nombre
+        private string conectionString = ConfigurationManager.ConnectionStrings["CadenaSITMAS"].ConnectionString;
+
+        #endregion
+
+        #region Propìedades
+
+        public int Id { get; set; }
+        public string Patente { get; set; }
+        public int Id_Modelo { get; set; }
+        public int Id_Tipo { get; set; }
+
+        #endregion
+
+        #region Metodos
+
+        public DataTable SelectAll()
+        {
+
+
+            string sqlSentencia = "sp_ListarVehiculo";
+
+            SqlConnection sqlCnn = new SqlConnection();
+            sqlCnn.ConnectionString = conectionString;
+
+            sqlCnn.Open();
+
+            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
+            sqlCom.CommandType = CommandType.StoredProcedure;
+
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = sqlCom;
+            da.Fill(ds);
+
+            sqlCnn.Close();
+
+            return ds.Tables[0];
+
+        }
+
+        //public DataTable SelectId()
+        //{
+
+
+        //    string sqlSentencia = "sp_VerEmpleadoId";
+
+        //    SqlConnection sqlCnn = new SqlConnection();
+        //    sqlCnn.ConnectionString = conectionString;
+
+
+        //    sqlCnn.Open();
+
+        //    SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
+        //    sqlCom.CommandType = CommandType.StoredProcedure;
+        //    sqlCom.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+
+        //    DataSet ds = new DataSet();
+
+        //    SqlDataAdapter da = new SqlDataAdapter();
+        //    da.SelectCommand = sqlCom;
+        //    da.Fill(ds);
+
+        //    sqlCnn.Close();
+
+        //    return ds.Tables[0];
+
+        //}
+
+        public void Insertar()
+        {
+
+            string sqlSentencia = "sp_InsertarVehiculo";
+
+            SqlConnection sqlCnn = new SqlConnection();
+            sqlCnn.ConnectionString = conectionString;
+
+            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
+            sqlCom.CommandType = CommandType.StoredProcedure;
+
+            sqlCom.Parameters.Add("@Patente", SqlDbType.NVarChar).Value = Patente;
+            sqlCom.Parameters.Add("@Id_Modelo", SqlDbType.Int).Value = Id_Modelo;
+            sqlCom.Parameters.Add("@Id_Tipo", SqlDbType.Int).Value = Id_Tipo;
+
+            sqlCnn.Open();
+
+            var res = sqlCom.ExecuteNonQuery();
+
+            sqlCnn.Close();
+
+        }
+
+
+        public void Modificar()
+        {
+
+
+            string sqlSentencia = "sp_ActualizarVehiculo";
+
+            SqlConnection sqlCnn = new SqlConnection();
+            sqlCnn.ConnectionString = conectionString;
+
+            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
+            sqlCom.CommandType = CommandType.StoredProcedure;
+
+            sqlCom.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+            sqlCom.Parameters.Add("@Patente", SqlDbType.NVarChar).Value = Patente;
+            sqlCom.Parameters.Add("@Id_Modelo", SqlDbType.Int).Value = Id_Modelo;
+            sqlCom.Parameters.Add("@Id_Tipo", SqlDbType.Int).Value = Id_Tipo;
+
+            sqlCnn.Open();
+
+            var res = sqlCom.ExecuteNonQuery();
+
+            sqlCnn.Close();
+
+        }
+
+
+        //EL MÉTODO BORRAR EMPLEADO SE ENCUENTRA FUNCIONANDO, PERO ESTÁ DESACTIVADO (COMENTADO)
+
+        public void Borrar()
+        {
+
+            string sqlSentencia = "sp_EliminarVehiculo";
+
+            SqlConnection sqlCnn = new SqlConnection();
+            sqlCnn.ConnectionString = conectionString;
+
+
+            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
+            sqlCom.CommandType = CommandType.StoredProcedure;
+
+            sqlCom.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+
+            sqlCnn.Open();
+
+            var res = sqlCom.ExecuteNonQuery();
+
+            sqlCnn.Close();
+
+
+        }
+
+
+        //public DataTable VistalistadoEmpleados()
+        //{
+
+        //    string sqlSentencia = "sp_ListarEmpleadosDetallados";
+
+        //    SqlConnection sqlCnn = new SqlConnection();
+        //    sqlCnn.ConnectionString = conectionString;
+
+        //    sqlCnn.Open();
+
+        //    SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
+        //    sqlCom.CommandType = CommandType.StoredProcedure;
+
+        //    DataSet ds = new DataSet();
+
+        //    SqlDataAdapter da = new SqlDataAdapter();
+        //    da.SelectCommand = sqlCom;
+        //    da.Fill(ds);
+
+        //    sqlCnn.Close();
+
+        //    return ds.Tables[0];
+
+        //}
+
+        #endregion
+    }
+}
